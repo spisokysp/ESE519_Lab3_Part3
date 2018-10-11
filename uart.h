4564437a6e27e3a4daef3597f0a6facd7544a7c4
spisokysp <40878319+spisokysp@users.noreply.github.com>
2018-10-12 03:06:35 +0800

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,48 @@
+#ifndef UART_H
+#define UART_H
+
+#define F_CPU 16000000UL
+#define BAUD 9600
+
+#include <util/setbaud.h>
+
+static int uart_putchar(char c, FILE *stream);
+static int uart_getchar(FILE *stream);
+void uart_init(void);
+
+static FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+static FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+
+int uart_putchar(char c, FILE *stream) {
+	if (c == '\n') {
+		uart_putchar('\r', stream);
+	}
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0 = c;
+	return 0;
+}
+
+int uart_getchar(FILE *stream) {
+	loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
+	return UDR0;
+}
+
+void uart_init(void) {
+	UBRR0H = UBRRH_VALUE;
+	UBRR0L = UBRRL_VALUE;
+
+	#if USE_2X
+	UCSR0A |= _BV(U2X0);
+	#else
+	UCSR0A &= ~(_BV(U2X0));
+	#endif
+
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+
+    // Redirect stdin and stdout so functions like printf and getchar work.
+	stdout = &uart_output;
+	stdin = &uart_input;
+}
+
+#endif // UART_H
